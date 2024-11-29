b97051b5100a5a2fcb571867b0dae1750a74d55a
sonsumin <sonsumin00@gmail.com>
2024-11-29 18:30:40 +0900

    Feat: 영속성 컨텍스트1

--- src/main/java/com/example/myjpa/hellojpa/JpaMain.java
+++ src/main/java/com/example/myjpa/hellojpa/JpaMain.java
@@ -2,6 +2,7 @@ package com.example.myjpa.hellojpa;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 
 public class JpaMain {
@@ -11,7 +12,33 @@ public class JpaMain {
 
         EntityManager em = emf.createEntityManager();
 
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
         //code
+
+        try {
+            Member member = new Member();
+            member.setId(1L);
+            member.setName("member1");
+
+            //영속
+            System.out.println("==BEFORE==");
+            em.persist(member);
+            Member findMEmber1 = em.find(Member.class, 1L);
+            Member findMEmber2 = em.find(Member.class, 1L);
+            System.out.println(findMEmber1==findMEmber2);
+
+            System.out.println("==AFTER==");
+
+
+            System.out.println("findMEmber = " + findMEmber1.getId());
+            System.out.println("findMEmber = " + findMEmber1.getName());
+        } catch (Exception e) {
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+
 
     }
 }
