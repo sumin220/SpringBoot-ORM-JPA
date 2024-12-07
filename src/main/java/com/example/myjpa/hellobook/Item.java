f1832a54e9a2b4e73683402b77c03bdcb5494faf
sonsumin <sonsumin00@gmail.com>
2024-12-07 22:50:31 +0900

    Feat: 기본 매핑 Item

---
+++ src/main/java/com/example/myjpa/hellobook/Item.java
@@ -0,0 +1,28 @@
+package com.example.myjpa.hellobook;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ITEM_ID")
+    private Long id;
+
+    private String name;
+
+    private int price;
+
+    private int stockQuantity;
+
+
+}
