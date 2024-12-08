7454f1561d80696aa272619e0b6d810447dd9b12
sonsumin <sonsumin00@gmail.com>
2024-12-08 11:35:19 +0900

    Feat: 기본 매핑 OrderItem

---
+++ src/main/java/com/example/myjpa/hellobook/OrderItem.java
@@ -0,0 +1,31 @@
+package com.example.myjpa.hellobook;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderItem {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ORDERITEM_ID")
+    private Long id;
+
+    @Column(name = "ORDER_ID")
+    private Long orderId;
+
+    @Column(name = "ITEM_ID")
+    private Long itemId;
+
+    private int orderPrice;
+
+    private int count;
+}
